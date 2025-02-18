423f2c3603252e5e7e3d50b1ce84f091ef3c37c9
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2025-02-18 08:08:57 +0100

    Add styling to website

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "../styles/Movie.css";
 
 function Movie() {
   const [movieId, setMovieId] = useState("");
@@ -9,8 +10,9 @@ function Movie() {
     e.preventDefault();
     navigate(`/movie/${movieId}`);
   }
+
   return (
-    <>
+    <div className="movie-container">
       <h1>Enter a Movie id</h1>
       <form onSubmit={handleMovieIdSubmit}>
         <input
@@ -22,7 +24,7 @@ function Movie() {
         <button type="submit">Watch</button>
       </form>
       <Link to={"/movie/339403"}>Watch sample movie</Link>
-    </>
+    </div>
   );
 }
 
