c1ef9b738fbb4a8acc5103781205706ea7688bf9
loris <buzzes_pallets5s@icloud.com>
2025-02-19 20:40:58 +0100

    I just have to test commit things

--- src/pages/WatchMovie.jsx
+++ src/pages/WatchMovie.jsx
@@ -12,13 +12,14 @@ function WatchMovie() {
   const [movieFile, setMovieFile] = useState(null);
   const [selectedProvider, setSelectedProvider] = useState(null);
 
+  // get movie data
   useEffect(() => {
     if (movieId) {
       fetchMovie(movieId)
-          .then((data) => {
-            setMovie(data);
-          })
-          .catch((error) => console.error(error));
+        .then((data) => {
+          setMovie(data);
+        })
+        .catch((error) => console.error(error));
     }
   }, [movieId]);
 
@@ -26,7 +27,7 @@ function WatchMovie() {
     if (movie) {
       if (selectedProvider === null) {
         setSelectedProvider(
-            movie.sources.find((source) => source.files[0].type === "hls")?.provider
+          movie.sources.find((source) => source.files[0].type === "hls")?.provider
         );
       }
     }
@@ -35,39 +36,38 @@ function WatchMovie() {
   useEffect(() => {
     if (selectedProvider) {
       setMovieFile(
-          movie.sources.find((source) => source.provider === selectedProvider)?.files[0]
+        movie.sources.find((source) => source.provider === selectedProvider)?.files[0]
       );
     }
   }, [selectedProvider, movie]);
 
   return (
-      <div className="watch-movie-container">
-        {movieFile ? (
-            movieFile.type === "hls" ? (
-                <VideoPlayer m3u8Url={movieFile?.file} subtitles={movie.subtitles} />
-            ) : (
-                <embed src={movieFile?.file} />
-            )
+    <div className="watch-movie-container">
+      {movieFile ? (
+        movieFile.type === "hls" ? (
+          <VideoPlayer m3u8Url={movieFile?.file} subtitles={movie.subtitles} />
         ) : (
-            <h1>Loading...</h1>
-        )}
+          <embed src={movieFile?.file} />
+        )
+      ) : (
+        <h1>Loading...</h1>
+      )}
 
-        {movie && (
-            <div className="provider-selection">
-              {movie.sources.map((source, index) => (
-                  <button
-                      key={index}
-                      className={`provider-button ${
-                          source.provider === selectedProvider ? "selected" : ""
-                      }`}
-                      onClick={() => setSelectedProvider(source.provider)}
-                  >
-                    {source.provider}
-                  </button>
-              ))}
-            </div>
-        )}
-      </div>
+      {movie && (
+        <div className="provider-selection">
+          {movie.sources.map((source, index) => (
+            <button
+              key={index}
+              className={`provider-button ${source.provider === selectedProvider ? "selected" : ""
+                }`}
+              onClick={() => setSelectedProvider(source.provider)}
+            >
+              {source.provider}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
